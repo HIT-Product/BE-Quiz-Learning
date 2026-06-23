300526254bee13d89abf028352a6ab488fd3a017
Nguyễn Văn Phước <vanphuoc0443@gmail.com>
2026-06-23 16:36:20 +0700

    style(models): format session schema

--- src/models/session.model.js
+++ src/models/session.model.js
@@ -1,30 +1,27 @@
 import mongoose, { model } from 'mongoose'
 
 const sessionSchema = new mongoose.Schema(
-    {
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        },
-        tokenHash: {
-            type: String,
-            required: true
-        },
-        expiresAt: {
-            type: Date,
-            required: true
-        }
-
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
     },
-    {
-        timestamps: true
+    tokenHash: {
+      type: String,
+      required: true
+    },
+    expiresAt: {
+      type: Date,
+      required: true
     }
+  },
+  {
+    timestamps: true
+  }
 )
 
 sessionSchema.index({ tokenHash: 1 })
 sessionSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 })
 
 export default model('Session', sessionSchema)
-
-
